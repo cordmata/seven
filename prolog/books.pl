8a53540c9b870c8d747f44414c15e30101898e7c
Matt Cordial <matt.cordial@gmail.com>
2012-09-26 20:35:38 -0700

    First prolog commit.

---
+++ prolog/books.pl
@@ -0,0 +1,23 @@
+wrote('Aldous Huxley', 'Brave New World').
+wrote('Aldous Huxley', 'The Doors of Perception').
+wrote('Tom Robbins', 'Jitterbug Perfume').
+wrote('Tom Robbins', 'Even Cowgirls Get the Blues').
+wrote('Tom Robbins', 'Skinny Legs and All').
+wrote('Cormac McCarthy', 'Blood Meridian').
+wrote('Cormac McCarthy', 'All the Pretty Horses').
+
+% Find books by an author.
+%
+% wrote('Aldous Huxley', Book).
+
+plays_instrument('Joe Pug', guitar).
+plays_instrument('Kasey Anderson', guitar).
+plays_instrument('Marc Ribot', guitar).
+plays_instrument('Dan Deacon', 808).
+plays_instrument('Victor Wooten', bass).
+
+performs('Joe Pug', folk).
+performs('Kasey Anderson', 'southern rock').
+performs('Dan Deacon', electronic).
+performs('Marc Ribot', jazz).
+performs('Victor Wooten', jazz).
---
+++ prolog/music.pl
@@ -0,0 +1,16 @@
+person('Aldous Huxley').
+person('Tom Robbins').
+person('Cormac McCarthy').
+
+wrote('Aldous Huxley', 'Brave New World').
+wrote('Aldous Huxley', 'The Doors of Perception').
+wrote('Tom Robbins', 'Jitterbug Perfume').
+wrote('Tom Robbins', 'Even Cowgirls Get the Blues').
+wrote('Tom Robbins', 'Skinny Legs and All').
+wrote('Cormac McCarthy', 'Blood Meridian').
+wrote('Cormac McCarthy', 'All the Pretty Horses').
+
+
+% Find books by an author.
+%
+% wrote('Aldous Huxley', Book).
