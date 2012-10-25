5e30a1eb5aff68d4cb01963390da53f5c9c0769d
Matt Cordial <matt.cordial@gmail.com>
2012-10-24 19:46:08 -0700

    Formatting

--- scala/tictactoe.scala
+++ scala/tictactoe.scala
@@ -1,31 +1,40 @@
 package seven
 
 object TicTacToe extends App {
-    val PLAYABLE_CHARS = List("X", "O")
+    val PLAYABLE_CHARS    = List("X", "O")
     val WINNING_POSITIONS = List( (0,1,2), (3,4,5), (6,7,8), (0,3,6),
                                   (1,4,7), (2,5,8), (0,4,8), (2,4,6) )
 
-    def isWinner(board: Array[String]): Tuple2[Boolean, String] = {
+    def checkWinner(board: Array[String]): Tuple2[Boolean, String] = {
         WINNING_POSITIONS.foreach { p =>
             val plays = List(board(p._1), board(p._2), board(p._3))
-            if( plays.forall(v => v == plays(0)) 
-                && (PLAYABLE_CHARS contains plays(0))) return (true, plays(0))
+            if( plays.forall(v => v == plays(0)) &&
+                (PLAYABLE_CHARS contains plays(0))) return (true, plays(0))
         }
         return (false, null)
     }
 
-    def testLists() = {
-      
-        val no = isWinner(Array( "X", " ", " ",
-		                         "X", " ", " ",
-		                         "0", " ", " "  ))
-                         
-        val yes = isWinner(Array( "O", " ", "X",
-		                          " ", "O", "X",
-		                          " ", " ", "O" ))
-        assert(no._1 == false)                 
-        assert(yes._1 == true && yes._2 == "O")
+    def testCheckWinner() = {
+
+        val no = Array(
+            "X", " ", " ",
+            "X", " ", " ",
+            "0", " ", " "
+        )
+
+        val yes = Array(
+            "O", " ", "X",
+            " ", "O", "X",
+            " ", " ", "O"
+        )
+
+        val (noAnswer, noChar)   = checkWinner(no)
+        val (yesAnswer, yesChar) = checkWinner(yes)
+
+        assert(noAnswer == false)
+        assert(yesAnswer && yesChar== "O")
+        println("Tests pass.")
     }
 
-    testLists
+    testCheckWinner
 }
