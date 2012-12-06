95df679c1275994131a7baa519b666d05448335e
Matt Cordial <matt.cordial@gmail.com>
2012-12-05 21:31:32 -0700

    Make to_ten prettier

--- erlang/day1.erl
+++ erlang/day1.erl
@@ -7,10 +7,10 @@ num_words("") -> 0;
 num_words(String) -> string:words(String).
 
 to_ten() -> to_ten(1).
-to_ten(Num) when Num =< 10 -> 
+to_ten(11) -> done;
+to_ten(Num) -> 
 	io:fwrite("~w~n", [Num]),
-	to_ten(Num + 1);
-to_ten(_) -> io:fwrite("Done~n").
+	to_ten(Num + 1).
 
 succeed(success) ->
 	io:fwrite("success~n");
