e87dc1ddce2d8dbcf422b63cfd9509a6a3ca3ad5
Matt Cordial <matt.cordial@gmail.com>
2012-12-05 22:20:14 -0700

    Add range.

--- erlang/day1.erl
+++ erlang/day1.erl
@@ -1,16 +1,20 @@
 -module(day1).
 -export([num_words/1]).
 -export([to_ten/0]).
+-export([range/2]).
+-export([range/1]).
 -export([succeed/1]).
 
 num_words("") -> 0;
 num_words(String) -> string:words(String).
 
-to_ten() -> to_ten(1).
-to_ten(11) -> done;
-to_ten(Num) -> 
-	io:fwrite("~w~n", [Num]),
-	to_ten(Num + 1).
+range(Num) -> range(0, Num).
+range(Num, Num) -> done;
+range(Start, End) -> 
+	io:fwrite("~w~n", [Start]),
+	range(Start + 1, End).
+
+to_ten() -> range(1, 11).
 
 succeed(success) ->
 	io:fwrite("success~n");
